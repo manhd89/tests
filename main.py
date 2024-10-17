b23e4541684d5c216b418487211403fba4f92332
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-10-18 05:13:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -249,8 +249,8 @@ def download_uptodown():
 def download_assets_from_repo(release_url):
     driver = create_chrome_driver()
     driver.get(release_url)
-    
-    downloaded_files = []
+
+    downloaded_files = {}
     
     try:
         WebDriverWait(driver, 15).until(
@@ -282,7 +282,7 @@ def download_assets_from_repo(release_url):
                 logging.info(
                     f"URL:{final_url} [{downloaded_size}/{total_size}] -> \"{filename}\" [1]"
                 )
-                downloaded_files.append(filename)  # Store downloaded filename
+                downloaded_files.setdefault(repo_info['repo'].replace("/", ""), []).append(filepath)  # Store downloaded filename
     except Exception as e:
         logging.error(f"Error while downloading from {release_url}: {e}")
     finally:
